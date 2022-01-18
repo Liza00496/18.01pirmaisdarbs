0d95fcb6c1d6991f475bad1189cad4c26f7a9f14
Liza00496 <77140885+Liza00496@users.noreply.github.com>
2022-01-18 10:39:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+teksts = input("Ievadiet skaitli: ")
+def countNumber(teksts):
+  summa = 0
+  for simbols in teksts:
+    summa = summa + int(simbols)
+    print(summa)
+  return summa
+countNumber(teksts)
